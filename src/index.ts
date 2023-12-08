bf0280f8d8d41815ba9772afff1015345843be8f
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-12-07 21:42:55 -0500

    FIX: error module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { CONFIG_PROJECT } from "@config/variablesDontenv";
-import { App, Server } from "./config";
+import { App, CONFIG_PROJECT, Server } from "./config";
 
 const serverApp = new Server({ config: CONFIG_PROJECT }, App);
 serverApp.start();
